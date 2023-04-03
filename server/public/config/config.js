e3900d3dd25c9ab6a1d850f070d31d3245032355
Diego Arndt <diegoarndt@outlook.com>
2023-04-02 23:36:56 -0400

    dotenv fix

--- server/public/config/config.js
+++ server/public/config/config.js
@@ -1,10 +1,15 @@
-require('dotenv').config();
+const apiKey = '6ee2887d8ef0af49a956931ffac86c0b';
+//2a4d321e462a454968098ab7e12fc3af
+//25e3dcc42b7508519df698b88599a569
+//6a024de9812b55fa41e921c7f5753291
+//4b98045c42932f028e130bfc111e4ea4
+//6bf800e3dc492c8b904b8378e95ae76d
 
 export const requestOptions = {
   method: 'GET',
   headers: {
     'x-rapidapi-host': 'v3.football.api-sports.io',
-    'x-rapidapi-key': process.env.API_KEY,
+    'x-rapidapi-key': apiKey,
   },
   redirect: 'follow',
 };
